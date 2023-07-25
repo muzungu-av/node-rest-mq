49e9a24639c1d949840f01a5864fea63107bea8f
muzungu.av <muzungu.av@gmail.com>
2023-07-25 13:42:51 +0300

    two http post endpoint (json, xml)

---
+++ http-node/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const jsonBodyParser = require("body-parser");
+const xmlparser = require('express-xml-bodyparser');
+
+const app = express();
+
+app.use(xmlparser());
+app.use(jsonBodyParser.urlencoded({ extended: true }));
+app.use(jsonBodyParser.json());
+
+/* Json запрос и ответ */
+app.post("/json", (req, res, next) => {
+    var body = req.body;
+    process.stdout.write(`Raw JSON: `); console.log(body); //в строку
+    console.log(`Parsed JSON: ` + JSON.stringify(body));
+
+    res.header("Content-Type", "application/json");
+    res.status(200).send({ answer: `I'm worked` });
+});
+/* обработка ошибок */
+
+/* Xml запрос и ответ */
+app.post("/xml", (req, res, next) => {
+    var body = req.body;
+    console.log('Raw XML: ' + req.rawBody);
+    console.log('Parsed XML: ' + JSON.stringify(req.body));
+
+    let data = '<?xml version="1.0" encoding="UTF-8"?>';
+    data += '<answer>';
+    data += `I'm worked`;
+    data += '</answer>';
+    res.header("Content-Type", "application/xml");
+    res.status(200).send(data);
+});
+
+/* обработка ошибок */
+app.use((error, req, res, next) => {
+    res.status(400);
+    res.json({ message: "ОШИБКА: " + error.message });
+})
+
+app.listen(8080);
