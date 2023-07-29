3988ee486f207b2af59e191b49642ee3ad242c9e
muzungu.av <muzungu.av@gmail.com>
2023-07-29 15:04:14 +0300

    task client

---
+++ task-node/index.js
@@ -0,0 +1,20 @@
+const amqp = require("amqplib");
+var channel, connection;
+
+connectQueue()  // call the connect function
+ 
+async function connectQueue() {
+    try {
+        connection = await amqp.connect("amqp://rabbitmq:rabbitmq@10.5.0.5");
+        channel    = await connection.createChannel()
+        
+        await channel.assertQueue("muzungu-queue")
+        
+        channel.consume("muzungu-queue", data => {
+            console.log(`${Buffer.from(data.content)}`);
+            channel.ack(data);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
